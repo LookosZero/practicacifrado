be5f35e1e978b2ab35158eae0cc87069b8c35aed
Lookos <lookoszero@gmail.com>
2024-10-15 14:03:09 +0200

    DesempaquetarFactura initial commit

--- src/main/java/practicacifrado/DesempaquetarFactura.java
+++ src/main/java/practicacifrado/DesempaquetarFactura.java
@@ -14,6 +14,20 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class DesempaquetarFactura {
     public static void main(String[] args) {
+
+        Security.addProvider(new BouncyCastleProvider());
+
+
+
+
+
         
     }
+
+    public static void mensajeAyuda() {
+		System.out.println("Desempaqueta la factura usando ");
+		System.out.println("\tSintaxis: java DesempaquetarFactura <nombre paquete> <fichero JSON factura> <ficheros con las claves necesarias>");
+		System.out.println();
+	}
+
 }
