2caa44bd5919e2f5b4606b0edbf2bf03a0a40d8d
Lookos <lookoszero@gmail.com>
2024-10-18 15:51:38 +0200

    Leer las cositas

--- src/main/java/practicacifrado/DesempaquetarFactura.java
+++ src/main/java/practicacifrado/DesempaquetarFactura.java
@@ -13,12 +13,52 @@ import javax.crypto.spec.*;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 public class DesempaquetarFactura {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        
+        if(args.length != 4){
+            mensajeAyuda();
+			return;
+        }
 
         Security.addProvider(new BouncyCastleProvider());
 
+        
+        Paquete paquete = new Paquete(args[0]);
+        
+        // Leer la factura cifrada del paquete
+        byte[] facturaCifrada = paquete.getContenidoBloque("facturaCifrada");
+        if(facturaCifrada == null){
+            System.out.println("El paquete no contiene la factura.");
+            return;
+        }
+
+        // Leer la clave DES cifrada del paquete
+        byte[] claveDESCifrada = paquete.getContenidoBloque("claveCifrada");
+        if(claveDESCifrada == null){
+            System.out.println("El paquete no contiene la clave cifrada.");
+            return;
+        }
+
+        // Leer la firma del paquete
+        byte[] firma = paquete.getContenidoBloque("firma");
+        if(firma == null){
+            System.out.println("El paquete no contiene la firma.");
+            return;
+        }
 
 
+        // Leer las claves de los ficheros
+        PrivateKey privateKeyHacienda = Utils.leerClavePrivada(args[2]);
+        if (privateKeyHacienda == null) {
+            System.out.println("El fichero no contiene la clave privada de hacienda.");
+            return;
+        }
+
+        PublicKey publicKeyEmpresa = Utils.leerClavePublica(args[3]);
+        if(publicKeyEmpresa == null){
+            System.out.println("El fichero no contiene la clave publica de la empresa.");
+            return;
+        }
 
 
         
@@ -26,7 +66,7 @@ public class DesempaquetarFactura {
 
     public static void mensajeAyuda() {
 		System.out.println("Desempaqueta la factura usando ");
-		System.out.println("\tSintaxis: java DesempaquetarFactura <nombre paquete> <fichero JSON factura> <ficheros con las claves necesarias>");
+		System.out.println("\tSintaxis: java DesempaquetarFactura <nombre paquete> <fichero JSON factura> <path clave privada hacienda> <path clave publica hacienda>");
 		System.out.println();
 	}
 
